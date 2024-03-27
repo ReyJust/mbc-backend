4ef86a62b044bed6d4f551276debb7c2f3e7c068
ReyJust <jusmayer@outlook.com>
2024-03-27 19:11:28 +0400

    feat: controller imports

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,3 +1,4 @@
 export * from "./auth/auth-controller";
-export * from "./bus-lines-controller";
+export * from "./bus-lines/bus-lines-controller";
 export * from "./bus-routes-controller";
+export * from "./user/user.controller";
